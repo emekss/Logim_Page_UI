78bbd5a6e27b58af8a9515888a6ff02d785a9d22
Chukwuemeka Kennedy <princeukaga@gmail.com>
2023-11-04 00:48:36 +0100

    welcome page added

---
+++ lib/util/journey_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class JourneyButton extends StatelessWidget {
+  const JourneyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: 261,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Center(
+          child: Text(
+        'JOURNEY',
+        style: GoogleFonts.poppins(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      )),
+    );
+  }
+}
